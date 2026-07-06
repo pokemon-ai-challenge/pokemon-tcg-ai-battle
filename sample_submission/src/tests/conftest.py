47ae57d32b4a923e1f631d7d9fd48d9a08aac374
Showgo1130 <shogo11301130@gmail.com>
2026-07-06 19:55:36 +0900

    restore sample_submission to 2787e1d state

--- sample_submission/src/tests/conftest.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-import sys
-
-
-SAMPLE_SUBMISSION_ROOT = Path(__file__).resolve().parents[2]
-if str(SAMPLE_SUBMISSION_ROOT) not in sys.path:
-    sys.path.insert(0, str(SAMPLE_SUBMISSION_ROOT))
